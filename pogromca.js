e75b37e60037fe1c81c2bf03270feb190857c363
akinhet <piotrzieniewicz2@gmail.com>
2026-01-24 22:26:45 +0100

    upgrades, people. upgrades

--- pogromca.js
+++ pogromca.js
@@ -1,78 +1,83 @@
 function extract_answers()
 {
-  const answers = document.getElementsByClassName("rightanswer");
-  const questions = document.getElementsByClassName("qtext");
-  const title = document.getElementsByClassName("page-header-headings");
+	const answers = document.getElementsByClassName("rightanswer");
+	const questions = document.getElementsByClassName("qtext");
+	const title = document.getElementsByClassName("page-header-headings");
 
-  const question_struct = [{title: title[0].children[0].innerText}];
+	const question_struct = [{title: title[0].children[0].innerText}];
 
-  for (let i = 0; i < answers.length; i++) {
+	for (let i = 0; i < answers.length; i++) {
 
-    const temp = answers[i].innerText.split('\n');
-    let str = "";
+		const temp = answers[i].innerText.split('\n');
+		let str = "";
 
-    for (let j = 0; j < temp.length; j++) {
-      switch (temp[j]) {
-        case 'Prawidłowymi odpowiedziami są:':
-        case 'Poprawna odpowiedź to:':
-        case '':
-          break;
+		for (let j = 0; j < temp.length; j++) {
+			switch (temp[j]) {
+				case 'Prawidłowymi odpowiedziami są:':
+				case 'Poprawna odpowiedź to:':
+				case '':
+					break;
 
-        case 'Poprawną odpowiedzią jest "Fałsz".':
-          str = str.concat("Fałsz");
-          break;
+				case 'Poprawną odpowiedzią jest "Fałsz".':
+					str = str.concat("Fałsz");
+					break;
 
-        case 'Poprawną odpowiedzią jest "Prawda".':
-          str = str.concat("Prawda");
-          break;
+				case 'Poprawną odpowiedzią jest "Prawda".':
+					str = str.concat("Prawda");
+					break;
 
-        default:
-          str = str.concat(temp[j]);
-          if (j != temp.length - 1)
-            str = str.concat("\n");
-          break;
-      }
-    }
+				default:
+					str = str.concat(temp[j]);
+					if (j != temp.length - 1)
+						str = str.concat("\n");
+					break;
+			}
+		}
+	
+		let question = questions[i].innerText.replace(/\n/g,'')
+		const img = questions[i].querySelector('img');
+		if (img)
+			question += ' ' + img.getAttribute('src').split('/').pop();
 
-    question_struct.push({question: questions[i].innerText.replace(/\n/g,''), answer: str});
-  }
+		question_struct.push({question: question, answer: str});
+	}
 
-  return question_struct;
+	return question_struct;
 }
 
 
 function print_answers(question_struct)
 {
-  console.log(question_struct[0].title);
+	console.log(question_struct[0].title);
 
-  for (let i = 1; i < question_struct.length; i++) {
-    console.log(question_struct[i].question, question_struct[i].answer);
-  }
+	for (let i = 1; i < question_struct.length; i++) {
+		console.log(question_struct[i].question, question_struct[i].answer);
+	}
 }
 
 
 async function send_answers(question_struct)
 {
-  let title = question_struct[0].title;
-  for (let i = 1; i < question_struct.length; i++) {
-    try {
-      const response = await fetch("https://pogromca.akinhet.xyz/api/questions", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: "question="+question_struct[i].question+"&answer="+question_struct[i].answer+"&title="+title,
-      });
+	let title = question_struct[0].title;
+	for (let i = 1; i < question_struct.length; i++) {
+		try {
+			const response = await fetch("https://pogromca.akinhet.xyz/api/questions", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded"
+				},
+				body: "question="+question_struct[i].question+"&answer="+question_struct[i].answer+"&title="+title,
+			});
 
-      const result = await response.json();
-      console.log("Success:", result);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
+			const result = await response.json();
+			console.log("Success:", result);
+		} catch (error) {
+			console.error("Error:", error);
+		}
+	}
 }
 
 
-//print_answers(extract_answers());
+print_answers(extract_answers());
 
-send_answers(extract_answers());
+// send_answers(extract_answers());
--- popup/search.js
+++ popup/search.js
@@ -1,47 +1,104 @@
 async function searchdb()
 {
-  const searchval = document.getElementById("search").value;
-  const answers = document.getElementById("answers");
+	const searchval = document.getElementById("search").value;
+	const answers = document.getElementById("answers");
 
-  const json = await fetch("https://pogromca.akinhet.xyz/api/questions?search="+searchval).then((resp) => resp.json());
+	const json = await fetch("https://pogromca.akinhet.xyz/api/questions?search="+searchval).then((resp) => resp.json());
 
 
-  answers.innerHTML = "";
-  for (let i = 0; i < json.length; i++) {
-    var child = document.createElement("div");
+	answers.innerHTML = "";
+	for (let i = 0; i < json.length; i++) {
+		var child = document.createElement("div");
 
-    child.classList.add("ans");
-    child.innerHTML = "<b>" + json[i].question + "</b><br><br> -> " + json[i].answer;
+		child.classList.add("ans");
+		child.innerHTML = "<b>" + json[i].question + "</b><br><br> -> " + json[i].answer;
 
-    answers.appendChild(child);
-  }
+		answers.appendChild(child);
+	}
+}
+
+// async function searchimg()
+// {
+// 	const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
+//
+// 	// Wykonujemy funkcję bezpośrednio na stronie użytkownika
+// 	browser.scripting.executeScript({
+// 		target: { tabId: tab.id },
+// 		func: () => {
+// 		// Ten kod uruchomi się na stronie (DOM)
+// 		const img = document.querySelector('img');
+// 		return img ? img.getAttribute('src') : "Nie znaleziono obrazka";
+// 		}
+// 	}).then(results => {
+// 		// Wynik wraca do popupu
+// 		document.getElementById('search').innerText = results[0].result;
+// 		console.log(results[0].result);
+// 		console.log("dupa");
+// 	});
+// }
+
+async function searchpage()
+{
+	const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
+
+	browser.scripting.executeScript({
+		target: { tabId: tab.id },
+		func: () => {
+			const questions = [];
+			const qs = document.querySelectorAll(".qtext");
+			qs.forEach((q) => {
+				const img = q.querySelector('img');
+				if (img)
+					questions.push(q.innerText.replace(/\n/g,'') + ' ' + img.getAttribute('src').split('/').pop());
+				else
+					questions.push(q.innerText.replace(/\n/g,''));
+			});
+
+			return questions;
+		}
+	}).then((questions) => {
+		const answers = document.getElementById("answers");
+		answers.innerHTML = "";
+		questions[0].result.forEach((q) => {
+			const json = await fetch("https://pogromca.akinhet.xyz/api/questions?search="+q).then((resp) => resp.json());
+			for (let i = 0; i < json.length; i++) {
+				var child = document.createElement("div");
+
+				child.classList.add("ans");
+				child.innerHTML = "<b>" + json[i].question + "</b><br><br> -> " + json[i].answer;
+
+				answers.appendChild(child);
+			}
+		});
+	});
 }
 
 
 document.addEventListener('input', searchdb);
+document.getElementById('image').addEventListener('click', searchpage);
 
 
 function sendMessageToSender(tabs)
 {
-  for (const tab of tabs) {
-    browser.tabs
-      .sendMessage(tab.id, { greeting: "Hi from background script" })
-      .then((response) => {
-        console.log("Message from the content script:");
-        console.log(response);
-		document.getElementById("search").value = response.response;
-		searchdb();
+	for (const tab of tabs) {
+		browser.tabs
+			.sendMessage(tab.id, { greeting: "Hi from background script" })
+			.then((response) => {
+				console.log("Message from the content script:");
+				console.log(response);
+		// document.getElementById("search").value = response.response;
+		searchpage();
 		console.log(document.getElementById("search").value);
-      });
-  }
+			});
+	}
 }
 
 
 window.addEventListener("load", () => {
-  browser.tabs
-    .query({
-      currentWindow: true,
-      active: true,
-    })
-    .then(sendMessageToSender);
+	browser.tabs
+		.query({
+			currentWindow: true,
+			active: true,
+		})
+		.then(sendMessageToSender);
 });
