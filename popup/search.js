8a2c3a9df0634762726840a7a23b10c4d57cb4fe
akinhet <piotrzieniewicz2@gmail.com>
2026-01-25 00:44:43 +0100

    oijlkm

--- popup/search.js
+++ popup/search.js
@@ -111,9 +111,9 @@ async function searchpage()
 	}).then(async (questions) => {
 		const answers = document.getElementById("answers");
 		answers.innerHTML = "";
-		for (int j = 0; j < questions[0].length; j++) {
+		for (let j = 0; j < questions[0].length; j++) {
 		// questions[0].result.forEach(async (q) => {
-			let q = questions[0][j]
+			let q = questions[0][j];
 			const json = await fetch("https://pogromca.akinhet.xyz/api/questions?search="+q).then((resp) => resp.json());
 			for (let i = 0; i < json.length; i++) {
 				var child = document.createElement("div");
