b1ca4c7648a6081fd023568a723fcf40a17b9b51
akinhet <piotrzieniewicz2@gmail.com>
2026-01-24 22:58:49 +0100

    chuj

--- pogromca.js
+++ pogromca.js
@@ -8,6 +8,39 @@ function getSmartText(element) {
     return clone.innerText.trim();
 }
 
+function getCleanText(element) {
+    // 1. Klonujemy element, żeby móc go modyfikować bez wpływu na stronę
+    const clone = element.cloneNode(true);
+
+    // 2. Znajdujemy wszystkie skrypty zawierające LaTeX
+    const mathScripts = clone.querySelectorAll('script[type^="math/tex"]');
+
+    mathScripts.forEach(script => {
+        // Pobieramy czysty wzór
+        const latex = script.textContent;
+        
+        // 3. KLUCZOWY MOMENT: Szukamy głównego kontenera tego wzoru.
+        // W Moodle jest to zazwyczaj klasa .filter_mathjaxloader_equation.
+        // Jeśli jej nie ma, bierzemy bezpośredniego rodzica skryptu.
+        const container = script.closest('.filter_mathjaxloader_equation') || script.parentNode;
+
+        // Tworzymy węzeł tekstowy z LaTeXem (dodajemy $ dla czytelności)
+        const textNode = document.createTextNode(` $${latex}$ `);
+
+        // 4. Podmieniamy CAŁY kontener (wraz z obrazkami/spanami MathJaxa) na sam tekst
+        if (container && container.parentNode) {
+            container.parentNode.replaceChild(textNode, container);
+        }
+    });
+
+    // 5. Usuwamy ewentualne pozostałości po podglądach (MathJax_Preview)
+    const previews = clone.querySelectorAll('.MathJax_Preview');
+    previews.forEach(el => el.remove());
+
+    // 6. Zwracamy czysty tekst, usuwając wielokrotne spacje i entery
+    return clone.innerText.replace(/\s+/g, ' ').trim();
+}
+
 
 function extract_answers()
 {
@@ -19,7 +52,7 @@ function extract_answers()
 
 	for (let i = 0; i < answers.length; i++) {
 
-		const temp = getSmartText(answers[i]).split('\n');
+		const temp = getCleanText(answers[i]).split('\n');
 		let str = "";
 
 		for (let j = 0; j < temp.length; j++) {
@@ -45,7 +78,7 @@ function extract_answers()
 			}
 		}
 	
-		let question = getSmartText(questions[i]).replace(/\n/g,'')
+		let question = getCleanText(questions[i]).replace(/\n/g,'')
 		const img = questions[i].querySelector('img');
 		if (img)
 			question += ' ' + img.getAttribute('src').split('/').pop();
